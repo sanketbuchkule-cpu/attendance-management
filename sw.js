7356032aeda79fd186252ad67f463ce50ce9ac00
sanketbuchkule-cpu <sanket.buchkule@dealeron.com>
2025-12-29 23:54:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,29 @@
-const CACHE_NAME = 'dealeron-v2'; // Changed version to force update
+// Changing the version number to v3 forces the browser to see this as a NEW app
+const CACHE_NAME = 'attendance-v3'; 
 
 self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Forces the new service worker to take over immediately
+    // This line forces the new, fixed code to take over immediately
+    self.skipWaiting(); 
+});
+
+self.addEventListener('activate', (event) => {
+    // This deletes the old "broken" cache automatically
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
+    // Always try the network first so redirects don't get stuck
     event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
