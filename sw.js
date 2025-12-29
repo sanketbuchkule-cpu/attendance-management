b05445d89245d525035a71c7265f1178d6fc4e5a
sanketbuchkule-cpu <sanket.buchkule@dealeron.com>
2025-12-29 23:47:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-const CACHE_NAME = 'dealeron-cache-v1';
-const urlsToCache = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'dealeron-v2'; // Changed version to force update
 
-// Install the service worker and cache the index page
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Forces the new service worker to take over immediately
 });
 
-// Serve the app from cache when offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
